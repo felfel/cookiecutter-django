9a0bb58703b0a80c011677356d10a7774db695bb
fahyik <fahyik@felfel.ch>
2018-07-24 11:03:30 +0200

    Add example models

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/example_module/models.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/example_module/models.py
@@ -1,3 +1,28 @@
+import uuid
+
 from django.db import models
 
+
 # Create your models here.
+class TimeStampedCreateOnlyModel(models.Model):
+
+    class Meta:
+        abstract = True
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class TimeStampedModel(TimeStampedCreateOnlyModel):
+
+    class Meta:
+        abstract = True
+
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class Example(TimeStampedModel):
+
+    class Meta:
+        db_name = 'example_module_example'
+
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
