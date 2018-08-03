1696190f380b6ceef16a7c9858d5956d1ec09e19
fahyik <fahyik@felfel.ch>
2018-08-03 10:56:33 +0200

    Add allowed_host setting for openshift

--- {{cookiecutter.project_slug}}/config/settings/production.py
+++ {{cookiecutter.project_slug}}/config/settings/production.py
@@ -11,6 +11,10 @@ from .base import env
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['{{ cookiecutter.domain_name }}'])
+# Hack to put in current host name, for Readiness check
+from socket import gethostbyname, gethostname  # noqa
+ALLOWED_HOSTS += [gethostbyname(gethostname()), ]
+
 
 # DATABASES
 # ------------------------------------------------------------------------------
