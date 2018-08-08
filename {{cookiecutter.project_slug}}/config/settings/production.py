4b96a7b79b4a312dddd7960089453593b088faa5
fahyik <fahyik@felfel.ch>
2018-08-08 14:54:37 +0200

    Use db connection pool for production

--- {{cookiecutter.project_slug}}/config/settings/production.py
+++ {{cookiecutter.project_slug}}/config/settings/production.py
@@ -19,8 +19,9 @@ ALLOWED_HOSTS += [gethostbyname(gethostname()), ]
 # DATABASES
 # ------------------------------------------------------------------------------
 DATABASES['default'] = env.db('DATABASE_URL')  # noqa F405
-DATABASES['default']['ATOMIC_REQUESTS'] = True  # noqa F405
-DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=60)  # noqa F405
+DATABASES['default']['ENGINE'] = 'django_db_geventpool.backends.postgresql_psycopg2'  # noqa F405
+DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=0)  # noqa F405
+
 
 # CACHES
 # ------------------------------------------------------------------------------
